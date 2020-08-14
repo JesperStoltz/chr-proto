011788cfc6453620ec32b2fe99ddd89514da1d4d
Jesper <jesper.stoltz@gmail.com>
2020-08-14 11:23:22 +0200

    Updates

---
+++ precache-manifest.bcf38b87b25057e1637bdac0650d84e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8687528dbe3cd38139d4d5452f8bfa69",
+    "url": "/chr-proto/index.html"
+  },
+  {
+    "revision": "0445a5d332d9f07ae481",
+    "url": "/chr-proto/static/css/main.af02244a.chunk.css"
+  },
+  {
+    "revision": "056c519179d36044da65",
+    "url": "/chr-proto/static/js/2.165208cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chr-proto/static/js/2.165208cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0445a5d332d9f07ae481",
+    "url": "/chr-proto/static/js/main.9fd2e0ef.chunk.js"
+  },
+  {
+    "revision": "1e21afcb9ed0d2d26b49",
+    "url": "/chr-proto/static/js/runtime-main.0ceeee58.js"
+  },
+  {
+    "revision": "0c59501c3f878058522402de06387e28",
+    "url": "/chr-proto/static/media/Neverwinter.0c59501c.otf"
+  },
+  {
+    "revision": "f32c75c93c56f397bde5c64e5e3874a7",
+    "url": "/chr-proto/static/media/bg.f32c75c9.png"
+  },
+  {
+    "revision": "3eba4ea5e36377a4c4aa81a9476546fc",
+    "url": "/chr-proto/static/media/button.3eba4ea5.png"
+  }
+]);
